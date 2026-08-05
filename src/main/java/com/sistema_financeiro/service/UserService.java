c0ee68f283a9722e4d0b8d3e73ee3470ca9c02ee
Lucas <124069740+Renzolucas@users.noreply.github.com>
2026-08-05 01:27:22 -0300

    fix: configurando service

--- src/main/java/com/sistema_financeiro/service/UserService.java
+++ src/main/java/com/sistema_financeiro/service/UserService.java
@@ -2,7 +2,12 @@ package com.sistema_financeiro.service;
 
 import org.springframework.stereotype.Service;
 
+import com.sistema_financeiro.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class UserService {
-    
+    private final UserRepository userRepository;
 }
