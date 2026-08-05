0733240d74103e1c3f040e3c689e3f2cc3d60166
Lucas <124069740+Renzolucas@users.noreply.github.com>
2026-08-05 01:27:02 -0300

    fix: ajustando Repository

--- src/main/java/com/sistema_financeiro/repository/UserRepository.java
+++ src/main/java/com/sistema_financeiro/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.sistema_financeiro.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sistema_financeiro.entity.user.user;
+@Repository
+public interface UserRepository extends JpaRepository<user, Integer>{
     
 }
