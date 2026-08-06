858b906c1c5f650042af9a5cce2151a48d23f4b1
Lucas <124069740+Renzolucas@users.noreply.github.com>
2026-08-06 17:58:57 -0300

    feat: adicionada parte logica do post

--- src/main/java/com/sistema_financeiro/service/UserService.java
+++ src/main/java/com/sistema_financeiro/service/UserService.java
@@ -1,7 +1,12 @@
 package com.sistema_financeiro.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sistema_financeiro.entity.user.dto.CreateUserBodyDTO;
+import com.sistema_financeiro.entity.user.user;
 import com.sistema_financeiro.repository.UserRepository;
 
 import lombok.AllArgsConstructor;
@@ -9,5 +14,21 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class UserService {
+    @Autowired
     private final UserRepository userRepository;
+
+    public user createUser(CreateUserBodyDTO dto){
+        user user = new user();
+        //VALIDAÇÃO DE EMAIL REPETIDO
+        Optional<user> emailRepetido = userRepository.findByEmail(dto.email());
+        if(emailRepetido.isPresent()){
+            throw new RuntimeException("Email já cadastrado");
+        }
+        user.setNome(dto.nome());
+        user.setEmail(dto.email()); 
+        user.setSenha(dto.senha());
+
+
+        return userRepository.save(user);
+    }
 }
