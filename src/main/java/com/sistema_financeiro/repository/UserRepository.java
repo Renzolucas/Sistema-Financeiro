3aad9ad8946089de69c1ad610267d577ee4c8e16
Lucas <124069740+Renzolucas@users.noreply.github.com>
2026-08-06 17:54:26 -0300

    fix: Ajustando anotação do repository

--- src/main/java/com/sistema_financeiro/repository/UserRepository.java
+++ src/main/java/com/sistema_financeiro/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.sistema_financeiro.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.sistema_financeiro.entity.user.user;
-@Repository
 public interface UserRepository extends JpaRepository<user, Integer>{
-    
+    Optional<user> findByEmail(String email);
 }
