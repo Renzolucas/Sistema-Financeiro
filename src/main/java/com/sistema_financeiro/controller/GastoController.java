f946042b14b529d0bfedf567ee48d2d78d69300b
Lucas <124069740+Renzolucas@users.noreply.github.com>
2026-08-14 20:49:04 -0300

    feat: adicionando funcionalidade ao controller de gasto

--- src/main/java/com/sistema_financeiro/controller/GastoController.java
+++ src/main/java/com/sistema_financeiro/controller/GastoController.java
@@ -1,5 +1,13 @@
 package com.sistema_financeiro.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/gastos")
+@AllArgsConstructor
 public class GastoController {
     
 }
