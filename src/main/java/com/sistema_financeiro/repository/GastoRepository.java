21f66db30289ba93e2104495e49ffc32136f5a8b
Lucas <124069740+Renzolucas@users.noreply.github.com>
2026-08-15 12:51:10 -0300

    feat: adicionando metodo no repository para buscar dentro de user a tabela user_id

--- src/main/java/com/sistema_financeiro/repository/GastoRepository.java
+++ src/main/java/com/sistema_financeiro/repository/GastoRepository.java
@@ -1,9 +1,11 @@
 package com.sistema_financeiro.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sistema_financeiro.entity.gasto.Gasto;
 
 public interface GastoRepository extends JpaRepository<Gasto, Integer> {
-    
+    List<Gasto> findByUserId(Integer id);
 }
