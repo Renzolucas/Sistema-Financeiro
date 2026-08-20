0fcc09643b014fcdbe4dba1026f288c2cca478c6
Lucas <124069740+Renzolucas@users.noreply.github.com>
2026-08-20 18:55:16 -0300

    feat: implementa autenticação de usuário com verificação de senha e geração de token

--- src/main/java/com/sistema_financeiro/controller/AuthController.java
+++ src/main/java/com/sistema_financeiro/controller/AuthController.java
@@ -1,8 +1,26 @@
 package com.sistema_financeiro.controller;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.sistema_financeiro.security.dto.LoginRequestDTO;
+import com.sistema_financeiro.security.dto.TokenResponseDTO;
+import com.sistema_financeiro.service.AuthService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+@RestController
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    
+    private final AuthService authService;
+
+
+    @PostMapping("/login")
+    public TokenResponseDTO authToken(@RequestBody @Valid LoginRequestDTO dto){
+        TokenResponseDTO token = authService.login(dto);
+        return token;
+    }
 }
--- src/main/java/com/sistema_financeiro/service/AuthService.java
+++ src/main/java/com/sistema_financeiro/service/AuthService.java
@@ -1,11 +1,33 @@
 package com.sistema_financeiro.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.sistema_financeiro.entity.user.User;
+import com.sistema_financeiro.repository.UserRepository;
+import com.sistema_financeiro.security.JwtUtil;
+import com.sistema_financeiro.security.dto.LoginRequestDTO;
+import com.sistema_financeiro.security.dto.TokenResponseDTO;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+
+    public TokenResponseDTO login(LoginRequestDTO dto){
+        User user = userRepository.findByEmail(dto.email())
+            .orElseThrow(()-> new RuntimeException("Email nao cadastrado"));
+
+        if(!passwordEncoder.matches(dto.senha(), user.getSenha())){//Verifica se a senha registrada junto a esse email e compativel com o existente no banco
+            throw new RuntimeException("Credenciais invalidas!");
+
+        }
+
+        String token = jwtUtil.gerarToken(dto.email());
+                return new TokenResponseDTO(token);
+    }
 }
